f22f11073ac9a2dd1d0be254e2285c028b0f2f64
DheniMoura <dhenifer@hotmail.com>
2023-01-15 11:28:13 -0300

    exercicio de sobrecarga

---
+++ quadrilatero/src/edu/orangetech/basecamp/Main.java
@@ -0,0 +1,14 @@
+package edu.orangetech.basecamp;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Quadrilatero.area(3.5);
+        Quadrilatero.area(5d, 1.2d);
+        Quadrilatero.area(5f, 1.2f);
+        Quadrilatero.area(2, 3,1.2);
+        Quadrilatero.area(2.5);
+        Quadrilatero.area(3d,1.5d);
+        Quadrilatero.area(3f,1.5f);
+        Quadrilatero.area(3,2,1);
+    }
+}
